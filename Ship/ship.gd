f5a9d1ca1ba96502ece5136e9d9cbd570d145327
ZaRdEr15 <73446410+ZaRdEr15@users.noreply.github.com>
2023-10-06 17:22:28 +0300

    Added acceleration/deceleration
    Ship movement and turning motion now has a friction, drift and is less snappy

--- Ship/ship.gd
+++ Ship/ship.gd
@@ -1,24 +1,58 @@
 extends CharacterBody2D
 
-@export var move_speed = 200.0
-@export var rotation_speed = 0.01
-var movement_direction = Vector2(1, 0)
+# Values for movement (can be changed in editor)
+@export var max_speed = 150.0
+@export var acceleration = 400.0
+@export var deceleration = 100.0
+@export var rotation_speed = 0.1
+@export var friction = 150.0
+@export var max_rotational_velocity = 0.8
 
+# Initial direction where ship is positioned
+var movement_direction = Vector2.RIGHT
+var last_rotation_direction = 0
 
 func _physics_process(delta):
+	# Save which input is pressed, can't allow to press both inputs at the same time
 	var propell_forward = Input.get_axis("move_back", "move_forward")
 	# Move towards where facing
-	if propell_forward:
-		velocity = movement_direction * propell_forward * move_speed
-	else:
-		velocity = Vector2(0, 0);
+	if propell_forward: # We move in the last direction that we looked at, either in positive (forward) or negative (backwards)
+		velocity += movement_direction * propell_forward * acceleration * delta
+		velocity = velocity.limit_length(max_speed)
+	else: # Over time if no input slow down the ship
+		if velocity.length() > (friction * delta):
+			velocity -= velocity.normalized() * (deceleration * delta)
+		else: # Stop the ship when fully slowed down
+			velocity = Vector2.ZERO
+	
 	
 	var rotation_direction = Input.get_axis("turn_left", "turn_right")
+	# Rotate in the pressed direction (CONSTANT SPEED, IMMEDIATE STOP)
 	if rotation_direction:
-		rotation += rotation_direction * rotation_speed
-		movement_direction = movement_direction.from_angle(rotation)
+		last_rotation_direction = rotation_direction
+		rotation += rotation_direction * rotational_acceleration(rotation_speed, delta) * delta
+		movement_direction = Vector2.from_angle(rotation)
+	else:
+		if rotation_speed > 0.1:
+			rotation += last_rotation_direction * rotational_deceleration(rotation_speed, delta) * delta
+			movement_direction = Vector2.from_angle(rotation)
+		else:
+			rotation = rotation
+			movement_direction = Vector2.from_angle(rotation)
 	
-	print(rotation_degrees)
-	print(movement_direction)
-	
+	#print(rotation_speed)
 	move_and_slide()
+
+
+func rotational_acceleration(curr_vel, delta):
+	if curr_vel > max_rotational_velocity:
+		return max_rotational_velocity
+	else:
+		rotation_speed = curr_vel + rotation_speed * delta
+		#print(rotation_speed)
+		return curr_vel
+		
+func rotational_deceleration(curr_vel, delta):
+	rotation_speed = curr_vel - rotation_speed * delta
+	print(rotation_speed)
+	return curr_vel
