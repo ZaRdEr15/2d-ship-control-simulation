f5c08bb13b13f2516f9816187949a5efdf478206
ZaRdEr15 <73446410+ZaRdEr15@users.noreply.github.com>
2023-10-16 01:16:29 +0300

    Added ability to switch maps

--- main.gd
+++ main.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -9,4 +8,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_just_pressed("restart_position"):
-		$Ship.position = Vector2(545, 283)
+		reset_ship_position()
+	elif  Input.is_action_just_pressed("switch_map_1"):
+		$TileMap.clear_layer(1)
+		reset_ship_position()
+	elif Input.is_action_just_pressed("switch_map_2"):
+		$TileMap.set_pattern(1, Vector2i(0, 0), $TileMap.tile_set.get_pattern(0))
+		reset_ship_position()
+
+func reset_ship_position():
+	$Ship.position = Vector2(545, 283)
