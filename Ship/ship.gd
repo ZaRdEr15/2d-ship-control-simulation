1386f327b964263b8f9d89d78b372470bb2fc2aa
ZaRdEr15 <73446410+ZaRdEr15@users.noreply.github.com>
2023-10-21 21:52:49 +0300

    Ship movement recording/playback

--- Ship/ship.gd
+++ Ship/ship.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-# Values for movement
+# Initial parameters for ship movement
 var max_speed = 150.0
 var acceleration = 400.0
 var deceleration = 100.0
@@ -10,16 +10,25 @@ var max_rotational_velocity = 0.8
 
 var config = ConfigFile.new()
 
-# Initial direction where ship is positioned
-var movement_direction = Vector2(0, 0)
+signal end_of_file
+
+var movement_direction = Vector2(0, 0) # Initial direction where ship is positioned
 var crash = false
 var last_rotation_direction = 0
 var rotation_direction_change = false
+
+var input_file : FileAccess
+var is_recording = false
+var is_playback = false
 
 func _ready():
 	rotation_degrees = -90
 	movement_direction = Vector2.from_angle(rotation)
 	ship_parameter_init()
+	var emitter = get_parent() # Main node
+	emitter.record_input.connect(_on_record_pressed)
+	emitter.playback_input.connect(_on_playback_pressed)
+	end_of_file.connect(_on_end_of_file_reached)
 
 func _physics_process(delta):
 	ship_movement(delta)
@@ -28,10 +37,16 @@ func _physics_process(delta):
 
 func ship_movement(delta):
 	# Save which input is pressed, can't allow to press both inputs at the same time
-	var propell_forward = Input.get_axis("move_back", "move_forward")
+	var propel_forward = Input.get_axis("move_back", "move_forward")
+	record_input(propel_forward, "UP", "DOWN")
+	if is_playback:
+		if input_file.eof_reached():
+			emit_signal("end_of_file")
+		else:
+			propel_forward = playback_input("UP", "DOWN")
 	# Move towards where facing
-	if propell_forward: # We move in the last direction that we looked at, either in positive (forward) or negative (backwards)
-		velocity += movement_direction * propell_forward * acceleration * delta
+	if propel_forward: # We move in the last direction that we looked at, either in positive (forward) or negative (backwards)
+		velocity += movement_direction * propel_forward * acceleration * delta
 		if !crash:
 			velocity = velocity.limit_length(max_speed)
 		else:
@@ -45,6 +60,12 @@ func ship_movement(delta):
 func ship_rotation(delta):
 	var rotation_direction = Input.get_axis("turn_left", "turn_right")
 	# Rotate in the pressed direction
+	record_input(rotation_direction, "RIGHT", "LEFT")
+	if is_playback:
+		if input_file.eof_reached():
+			emit_signal("end_of_file")
+		else:
+			rotation_direction = playback_input("RIGHT", "LEFT")
 	if rotation_direction:
 		# If you press other direction of rotation, start slowing down, before back to maximum speed
 		if rotation_direction != last_rotation_direction and rotation_direction_change == false:
@@ -65,7 +86,7 @@ func ship_rotation(delta):
 			rotation += last_rotation_direction * rotational_deceleration(rotation_speed, delta) * delta
 	movement_direction = Vector2.from_angle(rotation)
 
-	print(rotation_speed)
+	#print(rotation_speed)
 
 func rotational_acceleration(curr_vel, max_velocity, delta):
 	if curr_vel > max_velocity:
@@ -79,7 +100,7 @@ func rotational_deceleration(curr_vel, delta):
 	return curr_vel
 
 func ship_parameter_init():
-	var err = config.load("res://ship.cfg")
+	var err = config.load("res://Ship/ship.cfg")
 	if err != OK:
 		return
 	max_speed = config.get_value("Ship_Parameters", "max_speed")
@@ -88,6 +109,55 @@ func ship_parameter_init():
 	rotation_speed = config.get_value("Ship_Parameters", "rotation_speed")
 	friction = config.get_value("Ship_Parameters", "friction")
 	max_rotational_velocity = config.get_value("Ship_Parameters", "max_rotational_velocity")
+	
+func record_input(input_matching, input1, input2):
+	if is_recording:
+		var string
+		match input_matching:
+			1.0:
+				string = input1
+			-1.0:
+				string = input2
+			0.0:
+				string = "NONE"
+		input_file.store_line(string)
+		
+func playback_input(input1, input2):
+	var input = input_file.get_line()
+	match input:
+		input1:
+			input = 1.0
+		input2:
+			input = -1.0
+		"NONE":
+			input = 0.0
+	return input 
+
+func _on_record_pressed():
+	if is_recording:
+		# Create/open file and start saving input
+		input_file = FileAccess.open("user://recorded_input.save", FileAccess.WRITE)
+		if FileAccess.get_open_error() != OK:
+			return
+	else:
+		# Close file
+		input_file.close()
+		
+func _on_playback_pressed():
+	if is_playback:
+		# Read file for input
+		input_file = FileAccess.open("user://recorded_input.save", FileAccess.READ)
+		if FileAccess.get_open_error() != OK:
+			return
+	else:
+		# Close file
+		input_file.close()
+		
+func _on_end_of_file_reached():
+	input_file.close()
+	is_playback = false
+	var label = get_parent().get_node("Playback")
+	label.visible = false
 
 func _on_area_2d_body_entered(_body):
 	crash = true
--- main.gd
+++ main.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+signal record_input
+signal playback_input
+
 var starting_position = Vector2(545, 283)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -10,14 +13,18 @@ func _process(_delta):
 	Input.is_action_just_pressed("switch_map_3"):
 		reset_ship_position()
 	elif Input.is_action_just_pressed("hide_help"):
-		$Guide.visible = toggle_bool($Guide.visible)
+		$Guide.visible = !$Guide.visible
 	elif Input.is_action_just_pressed("show_ship_parameters"):
-		$ShipParameters.visible = toggle_bool($ShipParameters.visible)
+		$ShipParameters.visible = !$ShipParameters.visible
+	elif Input.is_action_just_pressed("record_input"):
+		$Ship.is_recording = !$Ship.is_recording
+		$Recording.visible = !$Recording.visible
+		emit_signal("record_input")
+	elif Input.is_action_just_pressed("playback_input"):
+		$Ship.is_playback = !$Ship.is_playback
+		$Playback.visible = !$Playback.visible
+		emit_signal("playback_input")
+		
 
 func reset_ship_position():
 	$Ship.position = starting_position
-	
-func toggle_bool(state):
-	if state:
-		return false
-	return true
