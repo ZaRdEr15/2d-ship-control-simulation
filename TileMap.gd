4b3409a1bc00ec649a237de2e5b799e9fc799b68
ZaRdEr15 <73446410+ZaRdEr15@users.noreply.github.com>
2023-11-06 14:00:24 +0200

    Changed approach to add different objects after map reloading

--- TileMap.gd
+++ TileMap.gd
@@ -4,11 +4,18 @@ var expression = Expression.new()
 
 var pattern_set = false
 
+# Preload each individual foreign ship
 var foreign_ship_v1 = preload("res://Ship/ForeignShipDownMov.tscn")
 var foreign_ship_v2 = preload("res://Ship/ForeignShipSquareMov.tscn")
 var foreign_ship_v3 = preload("res://Ship/ForeignShipDiagonalUpMov.tscn")
 
-var pos_arr = [Vector2i(9, -4), Vector2i(48, 1), Vector2i(43, 42)]
+# Declare new foreign ships here...
+
+# Foreign ships array, their version and corresponding position on the map (version to position is same index)
+var ship_arr = ["1", "2", "3", "1"]
+var pos_arr = [Vector2i(9, -4), Vector2i(48, 1), Vector2i(43, 42), Vector2i(14, -4)]
+# Add number and position to the arrays at the same index and modify the pattern in the tilemap to add new ships
+
 
 const START_STRING = "foreign_ship_v"
 
@@ -28,7 +35,7 @@ func _process(_delta):
 func add_foreign_ship_children():
 	if !get_children() and pattern_set:
 			for i in pos_arr.size():
-				var string_for_instance = START_STRING + str(i + 1) + ".instantiate()"
+				var string_for_instance = START_STRING + ship_arr[i] + ".instantiate()"
 				expression.parse(string_for_instance)
 				var foreign_ship_instance = expression.execute([], self)
 				foreign_ship_instance.position = map_to_local(pos_arr[i])
